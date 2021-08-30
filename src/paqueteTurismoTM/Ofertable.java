15781461a67ae524803d5ab5e7b14e5f1d3dd44b
1Hagi <dncabrera01@gmail.com>
2021-08-30 18:52:54 -0300

    Agregada la base de clase Ofertable
    Agregando solo una base de la Clase Ofertable para incorporar con las demás clases

---
+++ src/paqueteTurismoTM/Ofertable.java
@@ -0,0 +1,42 @@
+package paqueteTurismoTM;
+
+import java.util.ArrayList;
+
+public class Ofertable {
+
+	// De momento solo se puede crear un array de strings
+	static ArrayList<String> ofertasCopia = new ArrayList<String>();
+
+	// Y ademas todo lo que haga falta voy a estar rellenando con booleans
+	public static boolean comprobarCuposTotales() {
+		return false;
+	}
+
+	public static boolean hayOfertaDisponible(boolean esNuevoCliente) {
+		return false;
+	}
+
+	public static boolean getOferta(boolean unCliente) {
+		return false;
+	}
+
+	public static void resetearArrayCopia() {
+
+	}
+
+	public static void quitarOfertasSinCupo(boolean unCliente) {
+
+	}
+
+	public static void quitarOfertasQueNoPuedeComprar(boolean unCliente) {
+
+	}
+
+	public static void quitarOfertasYaCompradas(boolean unCliente) {
+
+	}
+
+	public static void quitarOfertasRechazadas(boolean unCliente) {
+
+	}
+}
