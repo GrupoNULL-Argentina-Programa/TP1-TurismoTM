b0f31e692e12583215970b0e0ad4afd3861c6cc8
1Hagi <dncabrera01@gmail.com>
2021-09-05 23:49:19 -0300

    Update PromocionAxB.java

--- src/paqueteTurismoTM/PromocionAxB.java
+++ src/paqueteTurismoTM/PromocionAxB.java
@@ -52,8 +52,8 @@ public class PromocionAxB extends Promocion {
 	@Override
 	public String toString() {
 		return "" + this.nombre + " contiene las siguientese atracciones " + "[" + tipoAtraccion + "]:" + "\n\t"
-				+ this.mostrarAtraccionesPagas() + "\n\tSu precio totol es de " + this.getCosto() + " monedas de oro."
-				+ "\n\t Además son gratis las atracciones:\n\t" + atracciones + "\n\tTiempo Total es de "
+				+ this.mostrarAtraccionesPagas() + "\n\tSu precio total es de " + this.getCosto() + " monedas de oro."
+				+ "\n\t Además son gratis las atracciones:\n\t" + this.atraccionesGratis + "\n\tTiempo Total es de "
 				+ this.getTiempo() + "hs.";
 	}
 }
