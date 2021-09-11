ef36ffb10933566ab47b123a975b7df528ffcc95
melijuarez <melijuarez_10@hotmail.com>
2021-09-10 21:04:56 -0300

    Metodo Cargar Promocion A x B

--- src/paqueteTurismoTM/App.java
+++ src/paqueteTurismoTM/App.java
@@ -5,34 +5,41 @@ import java.io.IOException;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		LectorDeFicheros lector = new LectorDeFicheros();
-		Archivo.clientes = lector.cargarClientes();
-		for (Cliente cliente : Archivo.clientes) {
-			System.out.println(cliente);
-		}
-
-		System.out.println();
-
-		LectorDeFicheros lector1 = new LectorDeFicheros();
-		Archivo.ofertas = lector1.cargarAtraccion();
-		for (Oferta atraccion : Archivo.ofertas) {
-			System.out.println(atraccion);
-		}
-		System.out.println();
-
-		LectorDeFicheros lector2 = new LectorDeFicheros();
-		Archivo.ofertas.addAll(lector2.cargarPromocionAbsoluta());
+//		LectorDeFicheros lector = new LectorDeFicheros();
+//		Archivo.clientes = lector.cargarClientes();
+//		for (Cliente cliente : Archivo.clientes) {
+//			System.out.println(cliente);
+//		}
+//
+//		System.out.println();
+//
+//		LectorDeFicheros lector1 = new LectorDeFicheros();
+//		Archivo.ofertas = lector1.cargarAtraccion();
+//		for (Oferta atraccion : Archivo.ofertas) {
+//			System.out.println(atraccion);
+//		}
+//		System.out.println();
+//
+//		LectorDeFicheros lector2 = new LectorDeFicheros();
+//		Archivo.ofertas.addAll(lector2.cargarPromocionAbsoluta());
+//		for (Oferta promocion : Archivo.ofertas) {
+//			System.out.println(promocion);
+//		}
+		
+		LectorDeFicheros lector3 = new LectorDeFicheros();
+		Archivo.ofertas.addAll(lector3.cargarPromocionAxB());
 		for (Oferta promocion : Archivo.ofertas) {
 			System.out.println(promocion);
 		}
 
+
 		// Ordena el array ofertas de mayor a menor y luego lo muestra.
 		//
-		Archivo.OrdenarOfertas();
-		System.out.println("\n\n");
-		for (Oferta a : Archivo.ofertas) {
-			System.out.println(a);
-		}
+//		Archivo.OrdenarOfertas();
+//		System.out.println("\n\n");
+//		for (Oferta a : Archivo.ofertas) {
+//			System.out.println(a);
+//		}
 		//
 	}
 }
--- src/paqueteTurismoTM/PromocionAxB.java
+++ src/paqueteTurismoTM/PromocionAxB.java
@@ -4,55 +4,47 @@ import java.util.ArrayList;
 
 public class PromocionAxB extends Promocion {
 
+	
 	public ArrayList<String> atraccionesGratis;
 
 	public PromocionAxB(String nombre, TipoAtraccion tipoAtraccion, ArrayList<String> atracciones,
-			ArrayList<Atraccion> atraccionesGratis) {
+			ArrayList<String> atraccionesGratis) {
 		super(nombre, tipoAtraccion, atracciones);
 		this.atraccionesGratis = new ArrayList<String>();
 	}
 
-	private ArrayList<String> mostrarAtraccionesPagas() {
-		ArrayList<String> atraccionesPagas = new ArrayList<String>();
-		for (String a : atracciones) {
-			atraccionesPagas.add(a);
-		}
-		for (int b = 0; b < atraccionesPagas.size(); b++) {
-			for (String c : atraccionesGratis) {
-				if (atraccionesPagas.get(b).equals(c)) {
-					atraccionesPagas.remove(b);
-				}
-			}
-		}
-		return atraccionesPagas;
-	}
+//	private ArrayList<String> mostrarAtraccionesPagas() {
+//		ArrayList<String> atraccionesPagas = new ArrayList<String>();
+//		for (String a : atracciones) {
+//			atraccionesPagas.add(a);
+//		}
+//		for (int b = 0; b < atraccionesPagas.size(); b++) {
+//			for (String c : atraccionesGratis) {
+//				if (atraccionesPagas.get(b).equals(c)) {
+//					atraccionesPagas.remove(b);
+//				}
+//			}
+//		}
+//		return atraccionesPagas;
+//	}
 
 	@Override
 	public int getCosto() {
-		int costoTotal = 0;
-		ArrayList<String> atraccionesPagas = new ArrayList<String>();
 		for (String a : atracciones) {
-			atraccionesPagas.add(a);
-		}
-		for (int b = 0; b < atraccionesPagas.size(); b++) {
-			for (String c : atraccionesGratis) {
-				if (atraccionesPagas.get(b).equals(c)) {
-					atraccionesPagas.remove(b);
+			for (Oferta b : Archivo.ofertas) {
+				if (a.equals(b.nombre)) {
+					costo += b.getCosto();
 				}
 			}
 		}
-		for (String a : atraccionesPagas) {
-			for (Oferta b : Archivo.ofertas)
-				if (a.equals(b.nombre))
-					costoTotal = b.getCosto();
-		}
-		return costoTotal;
+		return (int) costo;
 	}
-
+	
+	
 	@Override
 	public String toString() {
 		return "" + this.nombre + " contiene las siguientese atracciones " + "[" + tipoAtraccion + "]:" + "\n\t"
-				+ this.mostrarAtraccionesPagas() + "\n\tSu precio total es de " + this.getCosto() + " monedas de oro."
+				+ this.atracciones + "\n\tSu precio total es de " + this.getCosto() + " monedas de oro."
 				+ "\n\t Ademas son gratis las atracciones:\n\t" + this.atraccionesGratis + "\n\tTiempo Total es de "
 				+ this.getTiempo() + "hs.";
 	}
